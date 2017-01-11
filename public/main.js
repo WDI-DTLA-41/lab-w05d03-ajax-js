671b9a604c88997eff1af85dfa6e89ca8cf4f548
Justin Samuels <blizm87@gmail.com>
2017-01-11 13:31:02 -0800

    competed lab

--- public/main.js
+++ public/main.js
@@ -1,1 +1,32 @@
 console.log("May the odds be ever in your favor");
+
+var $readyBtn = $('#ready');
+var $challengeDiv = $('#challenges');
+var $nextBtn = $('<button>').attr('id','next').text('Next');
+var $moarDiv = $('<div>').attr('id', 'moar');
+
+$readyBtn.on('click', function(evt){
+  $.get('/challenges', function(page, status){
+    var html = '<ol>';
+    $challengeDiv.html(html);
+    page.forEach(function(ind){
+      html += '<li>' + ind.body + '</li>';
+    })
+    html += '</ol>';
+    $challengeDiv.html(html);
+    $challengeDiv.append($nextBtn);
+    $challengeDiv.append($moarDiv);
+  })
+})
+
+$nextBtn.on('click', function(evt){
+  $.get('/challenges?next=true', function(page, status){
+      var html = '<ol>';
+      $moarDiv.append(html);
+      page.forEach(function(ind){
+        html += '<li>' + ind.body + '</li>';
+      })
+      html += '</ol>';
+      $moarDiv.append(html);
+  })
+})
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+
 var app = express();
 
 // middleware
@@ -16,7 +17,24 @@ app.use(express.static(__dirname + '/public'));
 var challenges = require('./lib/challenges');
 // GET '/challenges' =>
 // Dynamic Routes
+app.get('/challenges', function(req, resp){
+  if(req.query.next === 'true'){
+    var nextTwo = []
+    nextTwo.push(challenges.shift());
+    nextTwo.push(challenges.shift());
+    resp.json(nextTwo);
+  } else {
+      var firstFour = [];
+      for(var i = 0; i < 4; i++){
+        firstFour.push(challenges.shift())
+      }
+      resp.json(firstFour);
+    }
+})
 
+//app.get('/challenges?next=true', function(req, resp){
+
+//})
 
 var port = 3000;
 app.listen(port, function(){
